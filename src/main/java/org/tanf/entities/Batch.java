f52abf5c0ae7e15d5d3c7d24c21e0a456e5d4951
abhishek-tf <Abhishek.RNair@informa.com>
2026-06-18 12:24:10 +0530

    Batch Entity

--- src/main/java/org/tanf/entities/Batch.java
+++ src/main/java/org/tanf/entities/Batch.java
@@ -1,4 +1,92 @@
 package org.tanf.entities;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+@Entity
+@Table(name="batch")
 public class Batch {
+    @Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="batch_id",nullable=false)
+    private Long batchId;
+	@Column(name="batch_name")
+	private String batchName;
+	private String technology ;
+	@Column(name="start_date")
+	private LocalDateTime startDate ;
+	@Column(name="end_date")
+	private LocalDateTime endDate ;
+	@ManyToOne
+	@JoinColumn(name = "trainer_id")
+	private Trainer trainer;
+
+    public Batch() {
+    }
+
+    public Batch(String batchName, LocalDateTime endDate, LocalDateTime startDate, String technology, Trainer trainer) {
+        this.batchName = batchName;
+        this.endDate = endDate;
+        this.startDate = startDate;
+        this.technology = technology;
+        this.trainer = trainer;
+    }
+
+    public int getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(int batchId) {
+        this.batchId = batchId;
+    }
+
+    public String getBatchName() {
+        return batchName;
+    }
+
+    public void setBatchName(String batchName) {
+        this.batchName = batchName;
+    }
+
+    public String getTechnology() {
+        return technology;
+    }
+
+    public void setTechnology(String technology) {
+        this.technology = technology;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public Trainer getTrainer() {
+        return trainer;
+    }
+
+    public void setTrainer(Trainer trainer) {
+        this.trainer = trainer;
+    }
+
+	
+
 }
---
+++ src/main/java/org/tanf/entities/Trainer.java
@@ -0,0 +1,5 @@
+package org.tanf.entities;
+
+public class Trainer {
+    
+}
